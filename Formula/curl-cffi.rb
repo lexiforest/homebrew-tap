242400757bbb73947607252631c2f98b571a2861
Yifei Kong <kongyifei@gmail.com>
2026-04-13 16:13:33 +0800

    Do not use source install for curl-cffi

--- Formula/curl-cffi.rb
+++ Formula/curl-cffi.rb
@@ -3,10 +3,19 @@ class CurlCffi < Formula
 
   desc "HTTP client CLI with browser impersonation support"
   homepage "https://github.com/lexiforest/curl_cffi"
-  url "https://files.pythonhosted.org/packages/48/5b/89fcfebd3e5e85134147ac99e9f2b2271165fd4d71984fc65da5f17819b7/curl_cffi-0.15.0.tar.gz"
   version "0.15.0"
-  sha256 "ea0c67652bf6893d34ee0f82c944f37e488f6147e9421bef1771cc6545b02ded"
   license "MIT"
+
+  stable do
+    on_arm do
+      url "https://files.pythonhosted.org/packages/83/2d/3915e238579b3c5a92cead5c79130c3b8d20caaba7616cc4d894650e1d6b/curl_cffi-0.15.0-cp310-abi3-macosx_11_0_arm64.whl"
+      sha256 "a25620d9bf989c9c029a7d1642999c4c265abb0bad811deb2f77b0b5b2b12e5b"
+    end
+    on_intel do
+      url "https://files.pythonhosted.org/packages/5e/42/54ddd442c795f30ce5dd4e49f87ce77505958d3777cd96a91567a3975d2a/curl_cffi-0.15.0-cp310-abi3-macosx_10_9_x86_64.whl"
+      sha256 "bda66404010e9ed743b1b83c20c86f24fe21a9a6873e17479d6e67e29d8ded28"
+    end
+  end
 
   depends_on "certifi" => :no_linkage
   depends_on "cffi" => :no_linkage
@@ -15,20 +24,6 @@ class CurlCffi < Formula
 
   pypi_packages package_name:     "curl-cffi",
                 exclude_packages: %w[certifi cffi]
-
-  # For curl-cffi, we actually download the binaries even when using source dist,
-  # There is really no need to use the tarball and build on user's machine.
-  if %w[arm64 aarch64].include?(RbConfig::CONFIG["host_cpu"])
-    resource "curl-cffi" do
-      url "https://files.pythonhosted.org/packages/83/2d/3915e238579b3c5a92cead5c79130c3b8d20caaba7616cc4d894650e1d6b/curl_cffi-0.15.0-cp310-abi3-macosx_11_0_arm64.whl"
-      sha256 "a25620d9bf989c9c029a7d1642999c4c265abb0bad811deb2f77b0b5b2b12e5b"
-    end
-  else
-    resource "curl-cffi" do
-      url "https://files.pythonhosted.org/packages/5e/42/54ddd442c795f30ce5dd4e49f87ce77505958d3777cd96a91567a3975d2a/curl_cffi-0.15.0-cp310-abi3-macosx_10_9_x86_64.whl"
-      sha256 "bda66404010e9ed743b1b83c20c86f24fe21a9a6873e17479d6e67e29d8ded28"
-    end
-  end
 
   resource "markdown-it-py" do
     url "https://files.pythonhosted.org/packages/5b/f5/4ec618ed16cc4f8fb3b701563655a69816155e79e24a17b651541804721d/markdown_it_py-4.0.0.tar.gz"
@@ -54,13 +49,7 @@ class CurlCffi < Formula
     python3 = "python3"
     venv = virtualenv_create(libexec, python3)
     venv.pip_install resources
-
-    resource("curl-cffi").fetch
-    args = std_pip_args(prefix: false, build_isolation: false).reject { |s| s == "--no-binary=:all:" }
-    system python3, "-m", "pip", "--python=#{venv.root}/bin/python",
-                          "install", *args, resource("curl-cffi").cached_download
-
-    bin.install_symlink libexec/"bin/curl-cffi"
+    venv.pip_install_and_link cached_download, build_isolation: false
   end
 
   test do
